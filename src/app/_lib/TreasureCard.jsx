5878064d4e17f73204718b8a9658b456af411526
ELR143 <emmlou143@gmail.com>
2024-01-10 10:32:23 +0000

    merge eveveryone's work, update gitingnore

---
+++ src/app/_lib/TreasureCard.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import api from "@/utils/api";
+
+export default function TreasureCard({ id }) {
+  const [percent, setPercent] = useState(null);
+
+  const treasure_id = Number(id);
+
+  const user_id =
+    typeof localStorage !== "undefined" && localStorage.getItem("user_id");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    api.getUserTreasureCollection(user_id).then((res) => {
+      let userTreasure = [...res.treasures, treasure_id];
+      api.patchProfileTreasure(user_id, userTreasure).then((res) => {
+        const identicalArr = userTreasure.every(
+          (element, index) => element === res.treasures[index]
+        );
+        if (identicalArr) {
+          api.IncrementTreasureCollectedBy(treasure_id).then((res) => {
+            console.log(res);
+          });
+        }
+      });
+    });
+  };
+
+  const treasureRarity = () => {
+    return api.getTreasureById(treasure_id).then((res) => {
+      return res.collected_by;
+    });
+  };
+
+  const totalUsers = () => {
+    return api.fetchData().then((res) => {
+      return res.length;
+    });
+  };
+
+  const calcPercentageFound = (treasure_id) => {
+    Promise.all([treasureRarity(treasure_id), totalUsers()]).then(
+      ([collected_by, total_users]) => {
+        const percentageFound = Math.floor((collected_by / total_users) * 100);
+        setPercent(percentageFound);
+      }
+    );
+  };
+
+  useEffect(() => {
+    calcPercentageFound(treasure_id);
+  }, [treasure_id]);
+
+  return (
+    <>
+      <button
+        className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Collect Treasure
+      </button>
+      <h1>Only {percent}% of pirates have found this treasure!</h1>
+    </>
+  );
+}
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import Card from "../_lib/Card";
 
 export default function HomePage() {
