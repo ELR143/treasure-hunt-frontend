7aaf4fe396d61b4e781367da60c2ea14ff020e8f
MHWatchy <mwatchy@gmail.com>
2024-01-11 08:50:29 +0000

    small bug fix

--- src/app/_lib/Filter.jsx
+++ src/app/_lib/Filter.jsx
@@ -22,11 +22,10 @@ export default function Filter({ setFilter }) {
           className="md:pl-1 ml-2 mr-2 rounded w-1/3 text-teal-800 font-bold first-line  h-8 "
           onChange={filterOnChange}
         >
-          <option value="">--Please choose an option--</option>
-          <option value="user_id-desc">Newest Pirates</option>
-          <option value="user_id-asc">Oldest Pirates</option>
           <option value="treasure_count-desc">Richest Pirates</option>
           <option value="treasure_count-asc">Poorest Pirates</option>
+          <option value="user_id-desc">Newest Pirates</option>
+          <option value="user_id-asc">Oldest Pirates</option>
         </select>
         <button className="w-auto md:ml-2 text-center mx-auto mt-1 px-2 py-1 tracking-wide text-teal-500 transition-colors duration-200 transform rounded focus:outline-none bg-white hover:bg-teal-500 hover:text-white cursor-pointer ">
           Apply
